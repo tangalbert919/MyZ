1b0e9cfee5448677e6fa552da65a7eca8053734d
DarknesGaming <epicdarknesgamer@gmail.com>
2016-09-23 22:37:03 -0500

    Minecraft 1.9 upgrade
    This upgrade will allow MyZ to function on any Minecraft server using
    Minecraft v1_9_R2 (1.9.4, basically). The upgrade isn't perfect, and you
    may encounter bugs.

---
+++ src/darknesgaming/nms/v1_9_R2/mobs/pathfinders/CustomEntitySelectorNearestAttackableTarget.java
@@ -0,0 +1,45 @@
+package darknesgaming.nms.v1_9_R2.mobs.pathfinders;
+
+import com.google.common.base.Predicate;
+
+import net.minecraft.server.v1_9_R2.EntityHuman;
+import net.minecraft.server.v1_9_R2.EntityLiving;
+import net.minecraft.server.v1_9_R2.EntityPlayer;
+import net.minecraft.server.v1_9_R2.MobEffectList;
+
+public class CustomEntitySelectorNearestAttackableTarget implements Predicate{
+
+    private final Predicate predicate;
+    private final CustomPathfinderGoalNearestAttackableTarget pathfinder;
+
+    public CustomEntitySelectorNearestAttackableTarget(CustomPathfinderGoalNearestAttackableTarget pathfinder, Predicate predicate) {
+        this.predicate = predicate;
+        this.pathfinder = pathfinder;
+    }
+
+    public boolean apply(EntityLiving entity) {
+        if (this.predicate != null && !this.predicate.apply(entity)) {
+            return false;
+        } else {
+            if (entity instanceof EntityHuman) {
+                if (((EntityPlayer) entity).playerInteractManager.isCreative() || entity.hasEffect(MobEffectList.WITHER) || entity.hasEffect(MobEffectList.INVISIBILITY)) {
+                    return false;
+                }
+
+                double range = (double) (((EntityHuman) entity).exp * 32.0F);
+
+                if ((double) entity.g(this.pathfinder.getE()) > range) {
+                    return false;
+                }
+            }
+
+            return this.pathfinder.a(entity, false);
+        }
+    }
+
+	@Override
+	public boolean apply(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}

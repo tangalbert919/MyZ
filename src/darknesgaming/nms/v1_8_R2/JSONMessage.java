9a2f0608130cfcfe59fd868543a66b3274011cc8
DarknesGaming <epicdarknesgamer@gmail.com>
2016-09-25 19:55:01 -0500

    MC 1.8.3 compatibility
    This will allow MyZ to work on Minecraft 1.8.3, and is part 2 of the
    complete MC 1.8.X compatibility upgrade.

---
+++ src/darknesgaming/nms/v1_8_R2/JSONMessage.java
@@ -0,0 +1,154 @@
+package darknesgaming.nms.v1_8_R2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import jordan.sicherman.json.JSONException;
+import jordan.sicherman.json.JSONStringer;
+import jordan.sicherman.nms.utilities.MessagePart;
+import jordan.sicherman.nms.utilities.NMS;
+import net.minecraft.server.v1_8_R2.IChatBaseComponent;
+import net.minecraft.server.v1_8_R2.NBTTagCompound;
+import net.minecraft.server.v1_8_R2.PacketPlayOutChat;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_8_R2.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class JSONMessage {
+
+    private final List messageParts = new ArrayList();
+
+    public JSONMessage(String firstPartText) {
+        this.messageParts.add(new MessagePart(firstPartText));
+    }
+
+    private MessagePart latest() {
+        return (MessagePart) this.messageParts.get(this.messageParts.size() - 1);
+    }
+
+    private void onClick(String name, String data) {
+        MessagePart latest = this.latest();
+
+        latest.clickActionName = name;
+        latest.clickActionData = data;
+    }
+
+    private void onHover(String name, String data) {
+        MessagePart latest = this.latest();
+
+        latest.hoverActionName = name;
+        latest.hoverActionData = data;
+    }
+
+    public JSONMessage achievementTooltip(String name) {
+        this.onHover("show_achievement", "achievement." + name);
+        return this;
+    }
+
+    public JSONMessage color(ChatColor color) {
+        if (color == null) {
+            return this;
+        } else if (!color.isColor()) {
+            throw new IllegalArgumentException(color.name() + " is not a color");
+        } else {
+            this.latest().color = color;
+            return this;
+        }
+    }
+
+    public JSONMessage command(String command) {
+        this.onClick("run_command", command);
+        return this;
+    }
+
+    public JSONMessage file(String path) {
+        this.onClick("open_file", path);
+        return this;
+    }
+
+    public JSONMessage itemTooltip(ItemStack itemStack) {
+        return itemStack != null && itemStack.getType() != Material.AIR ? this.itemTooltip(CraftItemStack.asNMSCopy(itemStack).save(new NBTTagCompound()).toString()) : this;
+    }
+
+    public JSONMessage itemTooltip(String itemJSON) {
+        this.onHover("show_item", itemJSON);
+        return this;
+    }
+
+    public JSONMessage link(String url) {
+        this.onClick("open_url", url);
+        return this;
+    }
+
+    public boolean send(Player player) {
+        try {
+            NMS.sendPacket(new PacketPlayOutChat(ChatModifier(this.toJSONString())), player);
+            return true;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return false;
+        }
+    }
+
+    private IChatBaseComponent ChatModifier(String jsonString) {
+		return null;
+	}
+
+	public JSONMessage style(ChatColor... styles) {
+        ChatColor[] achatcolor = styles;
+        int i = styles.length;
+
+        for (int j = 0; j < i; ++j) {
+            ChatColor style = achatcolor[j];
+
+            if (!style.isFormat()) {
+                throw new IllegalArgumentException(style.name() + " is not a style");
+            }
+        }
+
+        this.latest().styles = styles;
+        return this;
+    }
+
+    public JSONMessage suggest(String command) {
+        this.onClick("suggest_command", command);
+        return this;
+    }
+
+    public JSONMessage then(Object obj) {
+        this.messageParts.add(new MessagePart(obj.toString()));
+        return this;
+    }
+
+    public String toJSONString() {
+        JSONStringer json = new JSONStringer();
+
+        try {
+            if (this.messageParts.size() == 1) {
+                this.latest().writeJson(json);
+            } else {
+                json.object().key("text").value("").key("extra").array();
+                Iterator e = this.messageParts.iterator();
+
+                while (e.hasNext()) {
+                    MessagePart part = (MessagePart) e.next();
+
+                    part.writeJson(json);
+                }
+
+                json.endArray().endObject();
+            }
+        } catch (JSONException jsonexception) {
+            throw new RuntimeException("Invalid message");
+        }
+
+        return json.toString();
+    }
+
+    public JSONMessage tooltip(String text) {
+        this.onHover("show_text", text);
+        return this;
+    }
+}
---
+++ src/darknesgaming/nms/v1_8_R2/mobs/pathfinders/CustomPathfinderGoalRandomStroll.java
@@ -0,0 +1,69 @@
+package darknesgaming.nms.v1_8_R2.mobs.pathfinders;
+
+import net.minecraft.server.v1_8_R2.EntityCreature;
+import net.minecraft.server.v1_8_R2.PathfinderGoal;
+import net.minecraft.server.v1_8_R2.RandomPositionGenerator;
+import net.minecraft.server.v1_8_R2.Vec3D;
+
+public class CustomPathfinderGoalRandomStroll extends PathfinderGoal {
+
+    private final EntityCreature creature;
+    private double destX;
+    private double destY;
+    private double destZ;
+    private final double speed;
+    private int chanceToNot;
+    private boolean notPathing;
+
+    public CustomPathfinderGoalRandomStroll(EntityCreature creature, double speed) {
+        this(creature, speed, 120);
+    }
+
+    public CustomPathfinderGoalRandomStroll(EntityCreature creature, double speed, int chanceToNot) {
+        this.creature = creature;
+        this.speed = speed;
+        this.chanceToNot = chanceToNot;
+        this.a(1);
+    }
+
+    public boolean a() {
+        if (!this.notPathing) {
+            if (this.creature.bg() >= 100) {
+                return false;
+            }
+
+            if (this.creature.bb().nextInt(this.chanceToNot) != 0) {
+                return false;
+            }
+        }
+
+        Vec3D destination = RandomPositionGenerator.a(this.creature, 10, 7);
+
+        if (destination == null) {
+            return false;
+        } else {
+            this.destX = destination.a;
+            this.destY = destination.b;
+            this.destZ = destination.c;
+            this.notPathing = false;
+            return true;
+        }
+    }
+
+    public boolean b() {
+        return !this.creature.getNavigation().m();
+    }
+
+    public void c() {
+        this.creature.getNavigation().a(this.destX, this.destY, this.destZ, this.speed);
+    }
+
+    public void f() {
+        this.notPathing = true;
+    }
+
+    public void b(int paramInt) {
+        this.chanceToNot = paramInt;
+    }
+}
+
